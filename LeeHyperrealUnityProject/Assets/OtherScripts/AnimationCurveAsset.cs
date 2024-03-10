f7e0e73d5471fd6b118b3ad1b1cb681dd12cc44f
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2024-03-11 03:18:52 +1100

    adding assembly def for scripts

---
+++ LeeHyperrealUnityProject/Assets/OtherScripts/AnimationCurveAsset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RoR2
+{
+
+    [CreateAssetMenu(menuName = "RoR2/Animation Curve Asset")]
+    public class AnimationCurveAsset : ScriptableObject
+    {
+        public AnimationCurve value;
+    }
+}
+
--- LeeHyperrealUnityProject/Assets/Scripts/EffectScaler.cs
+++ LeeHyperrealUnityProject/Assets/Scripts/EffectScaler.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
-using RoR2;
+// using RoR2;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
